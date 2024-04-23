cb6f5773412209bc9eaf22ac87503751563bef47
aleeyazahraharmidi <aleeyaharmidi@gmail.com>
2024-04-23 20:41:44 +0800

    added app.run Lines

--- virtualWardrobe.py
+++ virtualWardrobe.py
@@ -1,1 +1,11 @@
-x = print("hello")
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+@app.route('/')
+@app.route('/home')
+def home():
+    return "Hello from Flask"
+
+if __name__ == '__main__':
+    app.run(debug=True)
