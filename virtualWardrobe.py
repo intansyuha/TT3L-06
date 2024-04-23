b21a57e37b282b553eb95886f4cff432345ffee1
aleeyazahraharmidi <aleeyaharmidi@gmail.com>
2024-04-23 21:30:20 +0800

    got bug so removed code idk

--- virtualWardrobe.py
+++ virtualWardrobe.py
@@ -3,14 +3,14 @@ from flask_wtf import FlaskForm
 from wtforms import FileField,  SubmitField
 from werkzeug.utils import secure_filename
 import os
-from wtforms.validators import InputRequired
+
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'clothesuploadkey'
 app.config['UPLOAD_FOLDER'] = 'static/files'
 
 class UploadClothesForm(FlaskForm):
-    file = FileField("File", validators=[InputRequired()])
+    file = FileField("File")
     submit = SubmitField("Upload File")
 
 @app.route('/', methods=['GET', "POST"])
