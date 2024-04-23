57e9a2ad8c8a13ba7b04187e3106f19146f3fcc5
Nur Intan Syuhadah binti Mohammad Mohsan <intansyuha95@gmail.com>
2024-04-24 01:08:09 +0800

    merged app.py with my new code

--- app.py
+++ app.py
@@ -2,18 +2,9 @@ from flask import Flask, render_template, request, redirect
 
 app = Flask(__name__)
 
-<<<<<<< HEAD
-# Dummy user database (replace this with your actual user database)
-users = {
-    "ali2": "pass123",
-    "user2": "password2",
-    "user3": "password3"
-}
-=======
 app = Flask(__name__, static_folder='static')
 
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///database.db'
->>>>>>> main
 
 @app.route('/')
 def login():
