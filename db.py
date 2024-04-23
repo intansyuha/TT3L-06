256cc66acfe285602ac4df0ca1e4de49226f4392
aleeyazahraharmidi <aleeyaharmidi@gmail.com>
2024-04-24 00:13:11 +0800

    tried to store image but code does not work idk what happened but i want to sleep will look at this tmr probably gn

--- db.py
+++ db.py
@@ -4,5 +4,6 @@ db = SQLAlchemy()
 
 def db_init(app):
     db.init_app(app)
-    with app.appcontext():
+
+    with app.app_context():
         db.create_all()
--- virtualWardrobe.py
+++ virtualWardrobe.py
@@ -4,13 +4,15 @@ from wtforms import FileField,  SubmitField
 from werkzeug.utils import secure_filename
 import os
 from wtforms.validators import InputRequired
-from db import db_init
+from db import db_init, db
+from models import Img
 
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'clothesuploadkey'
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///img.db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+app.config['UPLOAD_FOLDER'] = 'static/files'
 db_init(app)
 
 class UploadClothesForm(FlaskForm):
@@ -23,8 +25,18 @@ def home():
     form = UploadClothesForm()
     if form.validate_on_submit():
         file = form.file.data # Grab file
-        file.save(os.path.join(os.path.abspath(os.path.dirname(__file__)),app.config['UPLOAD_FOLDER'],secure_filename(file.filename))) # Save file
+        filename = secure_filename(file.filename)
+        file_path = os.path.join(app.config['UPLOAD_FOLDER'], filename) # Save file
+        file.save(file_path)
+        mimetype = file.content_type
+        with open(file_path, 'rb') as f:
+            img_data = f.read()
+        
+        img = Img(data=img_data(), mimetype=mimetype, name=filename)
+        db.session.add(img)
+        db.session.commit()
         return "File has been uploaded"
+    
     return render_template('index.html', form=form)
 
 if __name__ == '__main__':
