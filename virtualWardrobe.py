09cf381a1783e2dd9d3e7c65c48797dd2e91456f
aleeyazahraharmidi <aleeyaharmidi@gmail.com>
2024-04-23 21:19:16 +0800

    added save file codes

--- virtualWardrobe.py
+++ virtualWardrobe.py
@@ -1,10 +1,12 @@
 from flask import Flask, render_template #to return actual files
 from flask_wtf import FlaskForm
 from wtforms import FileField,  SubmitField
-
+from werkzeug.utils import secure_filename
+import os
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'clothesuploadkey'
+app.config['UPLOAD_FOLDER'] = 'static/files'
 
 class UploadClothesForm(FlaskForm):
     file = FileField("File")
@@ -14,6 +16,10 @@ class UploadClothesForm(FlaskForm):
 @app.route('/home', methods=['GET', "POST"])
 def home():
     form = UploadClothesForm()
+    if form.validate_on_submit():
+        file = form.file.data # Grab file
+        file.save(os.path.join(os.path.abspath(os.path.dirname(__file__)),app.config['UPLOAD_FOLDER'],secure_filename(file.filename))) # Save file
+        return "File has been uploaded"
     return render_template('index.html', form=form)
 
 if __name__ == '__main__':
