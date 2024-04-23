faedf44c90281d5bf172e1b613ba3aea430a34d3
aleeyazahraharmidi <aleeyaharmidi@gmail.com>
2024-04-23 21:00:46 +0800

    added lines in index.html created choose upload/upload file code

--- virtualWardrobe.py
+++ virtualWardrobe.py
@@ -1,11 +1,20 @@
 from flask import Flask, render_template #to return actual files
+from flask_wtf import FlaskForm
+from wtforms import FileField,  SubmitField
+
 
 app = Flask(__name__)
+app.config['SECRET_KEY'] = 'clothesuploadkey'
 
-@app.route('/')
-@app.route('/home')
+class UploadClothesForm(FlaskForm):
+    file = FileField("File")
+    submit = SubmitField("Upload File")
+
+@app.route('/', methods=['GET', "POST"])
+@app.route('/home', methods=['GET', "POST"])
 def home():
-    return "Hello from Flask"
+    form = UploadClothesForm()
+    return render_template('index.html', form=form)
 
 if __name__ == '__main__':
     app.run(debug=True)
