4784142b595a6e57958050a91448674e80e40f78
Nur Intan Syuhadah binti Mohammad Mohsan <intansyuha95@gmail.com>
2024-05-01 13:12:21 +0800

    modified user class which contains id, email, password, username. added unique=True on email and username so that it wont be duplicated

--- models.py
+++ models.py
@@ -4,6 +4,6 @@ from sqlalchemy.sql import func
 
 class User(db.Model, UserMixin):
     id = db.Column(db.Integer, primary_key=True)
-    username = db.Column(db.String(8), nullable=False)
-    password = db.Column(db.String(10), nullable=False)
-    email = db.Column(db.String(50), nullable=False)
+    username = db.Column(db.String(8), unique=True)
+    email = db.Column(db.String(150), unique=True)
+    password = db.Column(db.String(10))
