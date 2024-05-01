d3d8bc9f5742b757ef8953c0b01efb8b3b78147a
Nur Intan Syuhadah binti Mohammad Mohsan <intansyuha95@gmail.com>
2024-05-01 13:19:46 +0800

    created database

--- __init__.py
+++ __init__.py
@@ -1,7 +1,6 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from os import path
-from flask_login import LoginManager
 
 db = SQLAlchemy()
 DB_NAME = "database.db"
@@ -19,23 +18,14 @@ def create_app():
     app.register_blueprint(views, url_prefix='/')
     app.register_blueprint(auth, url_prefix='/')
 
-    from .models import User, Note
-    
-    with app.app_context():
-        db.create_all()
+    from .models import User
 
-    login_manager = LoginManager()
-    login_manager.login_view = 'auth.login'
-    login_manager.init_app(app)
-
-    @login_manager.user_loader
-    def load_user(id):
-        return User.query.get(int(id))
+    create_database(app)
 
     return app
 
 
 def create_database(app):
-    if not path.exists('website/' + DB_NAME):
+    if not path.exists('templates/' + DB_NAME):
         db.create_all(app=app)
         print('Created Database!')
