87bb7fc7e7ae35db023a6fe85c06aa0d733edff7
Nureen Adreana <nureenadreana88@gmail.com>
2024-05-07 00:03:59 +0800

    added code for dropdown (accordion) function

--- script.js
+++ script.js
@@ -1,9 +1,22 @@
-const dropdownLabels = document.querySelectorAll('.dropdown-row label');
+const labels = document.querySelectorAll('.accordion-row label');
 
-dropdownLabels.forEach(label => {
-    label.addEventListener('click', () => {
-        const dropdownBox = label.nextElementSibling;
-        dropdownBox.classList.toggle('show');
-        
-    })
-})
+// Initialize the display property of the accordion content
+labels.forEach(label => {
+  const accordionContent = label.nextElementSibling;
+  accordionContent.style.display = 'none'; // Initialize to 'none'
+});
+
+// Add event listener to each label
+labels.forEach(label => {
+  label.addEventListener('click', () => {
+    const accordionContent = label.nextElementSibling;
+    const chevron = label.querySelector('i');
+
+    // Toggle the display property of the accordion content
+    accordionContent.style.display = accordionContent.style.display === 'flex'? 'none' : 'flex';
+
+    // Toggle the chevron icon
+    chevron.classList.toggle('bx-chevron-right');
+    chevron.classList.toggle('bx-chevron-down');
+  });
+});
