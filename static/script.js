c5415e7ec6c9c9d3e089c079b17c68f100ae089d
Nureen Adreana <nureenadreana88@gmail.com>
2024-05-27 00:32:26 +0800

    troubleshoot the save function

--- static/script.js
+++ static/script.js
@@ -1,31 +1,9 @@
-//OUTFIT CREATOR//
-
-
-// Nav Bar //
-document.addEventListener("DOMContentLoaded", function() {
-  const navBar = document.querySelector("nav");
-  const menuBtns = document.querySelectorAll(".menu-icon");
-  const overlay = document.querySelector(".overlay");
-  
-  menuBtns.forEach((menuBtn) => {
-    menuBtn.addEventListener("click", () => {
-      navBar.classList.toggle("nav");
-      navBar.classList.toggle("open");
-    });
-  });
-  
-  overlay.addEventListener("click", () => {
-    navBar.classList.remove("nav");
-    navBar.classList.remove("open");
-  });
-});
-
-// Accordion //
+// Accordion functionality
 const labels = document.querySelectorAll('.accordion-row label');
 
 labels.forEach(label => {
   const accordionContent = label.nextElementSibling;
-  accordionContent.style.display = 'none'; 
+  accordionContent.style.display = 'none';
 });
 
 labels.forEach(label => {
@@ -33,16 +11,32 @@ labels.forEach(label => {
         const accordionContent = label.nextElementSibling;
         const chevron = label.querySelector('i');
 
-        // Toggle the accordion content
         accordionContent.style.display = accordionContent.style.display === 'flex' ? 'none' : 'flex';
 
-        // Toggle the chevron icon
         chevron.classList.toggle('bx-chevron-right');
         chevron.classList.toggle('bx-chevron-down');
     });
 });
 
-// Outfit selection n display function
+document.addEventListener("DOMContentLoaded", function() {
+  const navBar = document.querySelector("nav");
+  const menuBtns = document.querySelectorAll(".menu-icon");
+  const overlay = document.querySelector(".overlay");
+
+  menuBtns.forEach((menuBtn) => {
+    menuBtn.addEventListener("click", () => {
+      navBar.classList.toggle("nav");
+      navBar.classList.toggle("open");
+    });
+  });
+
+  overlay.addEventListener("click", () => {
+    navBar.classList.remove("nav");
+    navBar.classList.remove("open");
+  });
+});
+
+// Outfit selection and display functionality
 document.addEventListener("DOMContentLoaded", function () {
   const topSmallBoxes = document.querySelectorAll('#tops-accordion .sb-1 .sb');
   const topContainer = document.querySelector('.top-container');
@@ -67,7 +61,6 @@ document.addEventListener("DOMContentLoaded", function () {
   const accSmallBoxes = document.querySelectorAll('#accessories-accordion .sb');
   const accContainer = document.querySelector('.accessories-container');
   const accContainerImg = document.querySelector('.accessories-container img');
-
 
   topSmallBoxes.forEach((smallBox) => {
     smallBox.addEventListener('click', () => {
@@ -117,199 +110,158 @@ document.addEventListener("DOMContentLoaded", function () {
     });
   });
 
-
-    topContainer.addEventListener('click', () => {
+  topContainer.addEventListener('click', () => {
     topContainer.style.display = 'none';
   });
-    bottomContainer.addEventListener('click', () => {
+  bottomContainer.addEventListener('click', () => {
     bottomContainer.style.display = 'none';
   });
-    outerContainer.addEventListener('click', () => {
+  outerContainer.addEventListener('click', () => {
     outerContainer.style.display = 'none';
   });
-    shoeContainer.addEventListener('click', () => {
+  shoeContainer.addEventListener('click', () => {
     shoeContainer.style.display = 'none';
   });
-    bagContainer.addEventListener('click', () => {
+  bagContainer.addEventListener('click', () => {
     bagContainer.style.display = 'none';
   });
-    accContainer.addEventListener('click', () => {
+  accContainer.addEventListener('click', () => {
     accContainer.style.display = 'none';
   });
 });
 
-// Save the outfit
+// Save outfit functionality with name prompt
 document.addEventListener("DOMContentLoaded", function () {
     const saveButton = document.querySelector('.btn-save');
     saveButton.addEventListener('click', () => {
-        const selectedOutfit = {
-            top: document.querySelector('.top-container img').src,
-            bottom: document.querySelector('.bottom-container img').src,
-            outerwear: document.querySelector('.outerwear-container img').src,
-            shoes: document.querySelector('.shoes-container img').src,
-            bags: document.querySelector('.bags-container img').src,
-            accessories: document.querySelector('.accessories-container img').src
-        };
+        const outfitName = prompt('Enter the name of your outfit:');
 
-        let savedOutfits = JSON.parse(localStorage.getItem('savedOutfits')) || [];
-        savedOutfits.push(selectedOutfit);
-        localStorage.setItem('savedOutfits', JSON.stringify(savedOutfits));
+        if (outfitName !== null && outfitName.trim() !== '') {
+            const selectedOutfit = {
+                name: outfitName.trim(),
+                top: document.querySelector('.top-container img').src,
+                bottom: document.querySelector('.bottom-container img').src,
+                outerwear: document.querySelector('.outerwear-container img').src,
+                shoes: document.querySelector('.shoes-container img').src,
+                bags: document.querySelector('.bags-container img').src,
+                accessories: document.querySelector('.accessories-container img').src
+            };
+
+            let savedOutfits = JSON.parse(localStorage.getItem('savedOutfits')) || [];
+            savedOutfits.push(selectedOutfit);
+            localStorage.setItem('savedOutfits', JSON.stringify(savedOutfits));
+            alert(`Outfit "${outfitName}" saved!`);
+        } else {
+            alert('Please enter a valid outfit name.');
+        }
     });
 });
 
-
-// Save Button with outfit name
+// Sidebar and overlay functionality
 document.addEventListener("DOMContentLoaded", function () {
-  const saveButton = document.querySelector('#saveButton');
-        
-  saveButton.addEventListener('click', () => {
-    // Display the outfit name prompt
-    const outfitName = prompt('Enter the name of your outfit:');
+    const menuIcon = document.querySelectorAll('.menu-icon');
+    const sidebar = document.querySelector('.sidebar');
+    const overlay = document.querySelector('#overlay');
     
-    // Check if the user entered a valid outfit name
-    if (outfitName !== null && outfitName.trim() !== '') {
-      // Proceed to save the outfit with the entered name
-      alert(`Outfit "${outfitName}" saved!`);
-      // Here user can proceed to save the outfit with the given name
-    } else {
-      alert('Please enter a valid outfit name.');
-    }
-  });
-});
-
-
-
-// Gallery Outfit //
-
-// Sidebar //
-document.addEventListener("DOMContentLoaded", function () {
-  const menuIcon = document.querySelectorAll('.menu-icon');
-  const sidebar = document.querySelector('.sidebar');
-  const overlay = document.querySelector('#overlay');
-  
-  menuIcon.forEach(icon => {
-    icon.addEventListener('click', function () {
-      sidebar.classList.toggle('open');
-      overlay.classList.toggle('open');
+    menuIcon.forEach(icon => {
+        icon.addEventListener('click', function () {
+            sidebar.classList.toggle('open');
+            overlay.classList.toggle('open');
+        });
     });
-  });
-  
-  overlay.addEventListener('click', function () {
-    sidebar.classList.remove('open');
-    overlay.classList.remove('open');
-  });
-  
-  const accordionLabels = document.querySelectorAll('.accordion-row label');
-  accordionLabels.forEach(label => {
-          label.addEventListener('click', function () {
-              this.nextElementSibling.classList.toggle('hidden');
-            });
-          });
+    
+    overlay.addEventListener('click', function () {
+        sidebar.classList.remove('open');
+        overlay.classList.remove('open');
+    });
+    
+    const accordionLabels = document.querySelectorAll('.accordion-row label');
+    accordionLabels.forEach(label => {
+        label.addEventListener('click', function () {
+            this.nextElementSibling.classList.toggle('hidden');
+        });
+    });
 });
 
-// Toggle for public n private mode //
-  document.addEventListener("DOMContentLoaded", function () {
+// Gallery functionality
+document.addEventListener("DOMContentLoaded", function () {
     const toggleSwitches = document.querySelectorAll('.toggle-switch');
 
     toggleSwitches.forEach(switchElem => {
-      switchElem.addEventListener("click", () => {
-        switchElem.classList.toggle('active');
-      });
+        switchElem.addEventListener("click", () => {
+            switchElem.classList.toggle('active');
+        });
     });
-  });
+});
 
-// Outfit cards //
 document.addEventListener('DOMContentLoaded', () => {
-  const cardsContainer = document.getElementById('cardsContainer');
-  const savedOutfits = JSON.parse(localStorage.getItem('savedOutfits')) || [];
+    const cardsContainer = document.getElementById('cardsContainer');
+    const savedOutfits = JSON.parse(localStorage.getItem('savedOutfits')) || [];
 
-  // Create a new outfit card
-  function createOutfitCard(outfit) {
-    // Create the card container
-    const card = document.createElement('a');
-    card.href = '#';
-    card.className = 'card';
+    function createOutfitCard(outfit) {
+        const card = document.createElement('a');
+        card.href = '#';
+        card.className = 'card';
 
-    // Create the image element
-    const img = document.createElement('img');
-    img.src = outfit.top; // Use the top image from the saved outfit
-    img.alt = '';
+        const img = document.createElement('img');
+        img.src = outfit.top;
+        img.alt = '';
 
-    // Create the card body
-    const cardBody = document.createElement('div');
-    cardBody.className = 'card_body';
+        const cardBody = document.createElement('div');
+        cardBody.className = 'card_body';
 
-    // Create the card title
-    const cardTitle = document.createElement('h6');
-    cardTitle.className = 'card_title';
-    cardTitle.textContent = `Saved Outfit ${savedOutfits.indexOf(outfit) + 1}`;
+        const cardTitle = document.createElement('h6');
+        cardTitle.className = 'card_title';
+        cardTitle.textContent = outfit.name;
 
-    // Create the card options container
-    const cardOptions = document.createElement('div');
-    cardOptions.className = 'card_options';
+        const cardOptions = document.createElement('div');
+        cardOptions.className = 'card_options';
 
-    // Create the toggle switch
-    const toggleSwitch = document.createElement('div');
-    toggleSwitch.className = 'toggle-switch';
-    const switchSpan = document.createElement('span');
-    switchSpan.className = 'switch';
-    toggleSwitch.appendChild(switchSpan);
+        const toggleSwitch = document.createElement('div');
+        toggleSwitch.className = 'toggle-switch';
+        const switchSpan = document.createElement('span');
+        switchSpan.className = 'switch';
+        toggleSwitch.appendChild(switchSpan);
 
-    // Create the delete icon
-    const deleteDiv = document.createElement('div');
-    deleteDiv.className = 'delete';
-    const deleteIcon = document.createElement('i');
-    deleteIcon.className = 'bx bx-trash bx-sm';
-    deleteDiv.appendChild(deleteIcon);
+        const deleteDiv = document.createElement('div');
+        deleteDiv.className = 'delete';
+        const deleteIcon = document.createElement('i');
+        deleteIcon.className = 'bx bx-trash bx-sm';
+        deleteDiv.appendChild(deleteIcon);
 
-    // Append the toggle switch and delete icon to card options
-    cardOptions.appendChild(toggleSwitch);
-    cardOptions.appendChild(deleteDiv);
+        cardOptions.appendChild(toggleSwitch);
+        cardOptions.appendChild(deleteDiv);
 
-    // Append the title and options to the card body
-    cardBody.appendChild(cardTitle);
-    cardBody.appendChild(cardOptions);
+        cardBody.appendChild(cardTitle);
+        cardBody.appendChild(cardOptions);
 
-    // Append the image and card body to the card container
-    card.appendChild(img);
-    card.appendChild(cardBody);
+        card.appendChild(img);
+        card.appendChild(cardBody);
 
-    // Append the card to the cards container
-    cardsContainer.appendChild(card);
-  }
-
-
-  // Display saved outfits
-  savedOutfits.forEach(outfit => {
-    createOutfitCard(outfit);
-  });
-
-  // Delete Function //
-  cardsContainer.addEventListener('click', function(event) {
-    if (event.target.classList.contains('bx-trash')) {
-      const card = event.target.closest('.card');
-
-      // Identify the outfit data associated with the deleted card
-      const imageUrl = card.querySelector('img').src;
-      const outfitIndex = savedOutfits.findIndex(outfit => outfit.top === imageUrl);
-      if (outfitIndex !== -1) {
-        const outfitName = card.querySelector('.card_title').textContent;
-        // Remove the outfit data from the array
-        savedOutfits.splice(outfitIndex, 1);
-        // Update local storage with the modified array
-        localStorage.setItem('savedOutfits', JSON.stringify(savedOutfits));
-        card.remove();
-
-        // Alert User that outfit has been deleted
-        alert('Outfit has been deleted!')
-      }
+        cardsContainer.appendChild(card);
     }
-  });
 
-  // Add event listener for the toggle switch functionality
-  cardsContainer.addEventListener('click', function(event) {
-    if (event.target.classList.contains('switch')) {
-      event.target.closest('.toggle-switch').classList.toggle('active');
-    }
-  });
+    savedOutfits.forEach(outfit => {
+        createOutfitCard(outfit);
+    });
+
+    cardsContainer.addEventListener('click', function(event) {
+        if (event.target.classList.contains('bx-trash')) {
+            const card = event.target.closest('.card');
+            const imageUrl = card.querySelector('img').src;
+            const outfitIndex = savedOutfits.findIndex(outfit => outfit.top === imageUrl);
+            if (outfitIndex !== -1) {
+                savedOutfits.splice(outfitIndex, 1);
+                localStorage.setItem('savedOutfits', JSON.stringify(savedOutfits));
+                card.remove();
+                alert('Outfit has been deleted!');
+            }
+        }
+    });
+
+    cardsContainer.addEventListener('click', function(event) {
+        if (event.target.classList.contains('switch')) {
+            event.target.closest('.toggle-switch').classList.toggle('active');
+        }
+    });
 });
