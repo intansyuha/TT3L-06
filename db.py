ce669c1f66f787c73deffac0bfdb577496f76e44
aleeyazahraharmidi <aleeyaharmidi@gmail.com>
2024-06-03 16:04:57 +0800

    deleted unnecessary files added delete image button feature

--- db.py
+++ db.py
@@ -1,30 +1,3 @@
-from flask import Flask
-from flask_sqlalchemy import SQLAlchemy
-
-app = Flask(__name__)
-app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///outfits.db"
-db = SQLAlchemy(app)
-
-
-# Define your Outfit model here
-class Outfit(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    outfit_name = db.Column(db.String(100))
-    top_color = db.Column(db.String(20))
-    bottom_color = db.Column(db.String(20))
-    outer_color = db.Column(db.String(20))
-    shoe_color = db.Column(db.String(20))
-    acc_color = db.Column(db.String(20))
-
-
-def init_db():
-    with app.app_context():
-        db.create_all()
-
-
-if __name__ == "__main__":
-    init_db()
-
 from flask_sqlalchemy import SQLAlchemy
 
 db = SQLAlchemy()
@@ -35,3 +8,4 @@ def db_init(app):
 
     with app.app_context():
         db.create_all()
+
--- models.py
+++ models.py
@@ -1,13 +1,4 @@
-from .db import db
-from flask_login import UserMixin
-from sqlalchemy.sql import func
-
-class User(db.Model, UserMixin):
-    id = db.Column(db.Integer, primary_key=True)
-    email = db.Column(db.String(150), unique=True)
-    password = db.Column(db.String(150))
-    username = db.Column(db.String(150))
-    date = db.Column(db.DateTime(timezone=True), default=func.now())
+from db import db
 
 class Img(db.Model):
     id = db.Column(db.Integer, primary_key=True)
@@ -15,3 +6,4 @@ class Img(db.Model):
     name = db.Column(db.Text, nullable=False)
     mimetype = db.Column(db.Text, nullable=False)
     category = db.Column(db.String(50), nullable=False)
+
